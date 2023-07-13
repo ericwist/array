9dfffcaf8a862d1ec78204394fb9085e6708638c
Eric Wistrand <eric.j.wistrand@gmail.com>
2023-07-12 23:02:25 -0700

    1. Update to use any type (fully type agnostic) 2. Add desc sort 3. Add "move zeros" to beginning of arr

--- array.h
+++ array.h
@@ -1,8 +1,10 @@
-// This is a type agnostic array class
+// Array functions
 // Author: Eric Wistrand
 // Date  : 11/28/2018
+// Updated : 07/12/2023
 
 #pragma once
+#include <algorithm>
 #include <iostream>
 #include <cstdlib>
 #include <array>
@@ -14,15 +16,16 @@ using namespace std;
 template<typename T> class CArray {
 
 private:
-    int size;
-    T *m_array;
-
+    int len;
+    T* m_array;
+    
 public:
     CArray(int s)
-        :size(s)
+        :len(s)
     {
-        m_array = new T[size];
-        memset(m_array, 0, size*(sizeof(m_array)/sizeof(*m_array)));
+        m_array = new T[len];
+        memset(m_array, 0, len * (sizeof(m_array) / sizeof(*m_array)));
+        
     }
 
     ~CArray() {
@@ -33,21 +36,23 @@ public:
         m_array[elem] = val;
     }
 
+    //print array in order
     void dumpArray() {
-        cout << "Dump contents of array" << endl;
-        for (int i = 0; i < size; ++i) {
+        cout << "Print contents of array" << endl;
+        for (int i = 0; i < len; ++i) {
             cout << m_array[i] << " ";
         }
-        cout << endl;
+        cout << endl << endl;
     }
 
+    //reverse array
     void reverseArray() {
-        if (m_array == nullptr){
+        if (m_array == nullptr) {
             return;
         }
         T temp;
-        T *e = &m_array[size - 1];
-        T *b = &m_array[0];
+        T* e = &m_array[len - 1];
+        T* b = &m_array[0];
 
         while (e > b) {
             temp = *e;
@@ -56,51 +61,73 @@ public:
         }
         return;
     }
-    //only for integers
-     struct{
-        bool operator()(const T a,const T b) const
+
+    //custom sort descending
+    struct {
+        bool operator()(const T a, const T b) const
         {
-            return a < b;
+            return b < a;
         }
-    }customLess;
+    }customDesc;
+
+    //sort desc
+    void dSort() {
+        sort(m_array, m_array + len, customDesc);
+    }
+
+    //sort asc, default
     void Sort() {
-         sort(m_array, m_array + size, customLess);
+        sort(m_array, m_array + len);
     }
-    // this works for both string and int.  
-    void bubbleSort() {
-        T temp;
-        for (int i = 1; i < size; i++) {
-            for (int j = (size - 1); j >= i; j--) {
-                if (m_array[j-1] > m_array[j]) {
-                    temp = m_array[j - 1];
-                    m_array[j - 1] = m_array[j];
-                    m_array[j] = temp;
-                }
-            }
-        }
-    }
+
     //move all zeros to the end of the array
     int moveZerosToEnd() {
+        int retcnt = 0;
         if (m_array == nullptr) {
-            return 0;
+            return retcnt;
         }
         int count = 0;
-        for (int i = 0; i < size; ++i)
-        { 
+        for (int i = 0; i < len; ++i)
+        {
             if (m_array[i] != 0)
             {
                 m_array[count++] = m_array[i];
             }
         }
-        int ret = size - count;
-        while (count < size)
+        retcnt = len - count;
+        while (count < len)
         {
             m_array[count++] = 0;
         }
-        return ret;
+        return retcnt;
     }
+
+    //move all zero to beginning
+    //ret num zeros
+    int moveZerosToBeginning() {
+        int retcnt = 0;
+        if (m_array == nullptr) {
+            return retcnt;
+        }
+        int count = len - 1;
+        for (int i = len - 1; i > -1; --i)
+        {
+            if (m_array[i] != 0)
+            {
+                m_array[count--] = m_array[i];
+            }
+        }
+
+        while (count >= 0)
+        {
+            m_array[count--] = 0;
+            ++retcnt;
+        }
+        return retcnt;
+    }
+
+};
     //compare
     //insert element into arrary increase memory
-};
 
 
